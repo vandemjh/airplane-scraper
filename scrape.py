ac8fec061501be90cda1368a5f69e678aa0c40bf
vandemjh <jack.vandem@gmail.com>
2020-03-15 04:11:32 -0400

    testing out charts

--- scrape.py
+++ scrape.py
@@ -1,22 +1,38 @@
 #!/usr/bin/python3
 
 # Logs all html available through terrible airplane wifi
-# Requires requests
 import requests
 import json
+import reportlab
 
 airplane = open("airplane.json","w")
 
 toReturn = {}
-for i in range(100):
+for i in range(1):
     URL = "https://www.aainflight.com/media/" + str(i)
     page = requests.get(URL)
     if (page.status_code == 200):
         contentSplit = str(page.content).split()
         for word in contentSplit:
-            if not word in toReturn:
-                toReturn[word] = 1
-            else:
-                toReturn[word] += 1
+            # This is hacky
+            for inWord in word.split("\n"):
+                for inInWord in inWord.split("\t"):
+                    if not word in toReturn:
+                        toReturn[inInWord] = 1
+                    else:
+                        toReturn[inInWord] += 1
+
+largestVal = 0
+for val in toReturn.values():
+    if (val > largestVal):
+        largestVal = val
 
 json.dump(toReturn, airplane, sort_keys=True)
+
+chart = ScatterPlot()
+chart.data = toReturn
+chart.width      = 115
+chart.height     = 80
+chart.x          = 30
+chart.y          = 40
+chart.save()
