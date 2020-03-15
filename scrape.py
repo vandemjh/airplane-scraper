a635da0ecc8864f6ea2c4cacd60c7893e3aeefa5
vandemjh <jack.vandem@gmail.com>
2020-03-15 03:29:19 -0400

    script rough draft to pull from first 100 pages

--- scrape.py
+++ scrape.py
@@ -3,9 +3,21 @@
 import os
 import requests
 import sys
+import json
 
+airplane = open("airplane.json","w")
 
-URL = "https://www.aainflight.com/media/1"
-page = requests.get(URL)
-print(page.status_code)
-print(page.content)
+toReturn = {}
+count = 0
+for i in range(100):
+    URL = "https://www.aainflight.com/media/" + str(i)
+    page = requests.get(URL)
+    if (page.status_code == 200):
+        contentSplit = str(page.content).split()
+        for word in contentSplit:
+            if not word in toReturn:
+                toReturn[word] = 1
+            else:
+                toReturn[word] += 1
+
+json.dump(toReturn, airplane, sort_keys=True)
