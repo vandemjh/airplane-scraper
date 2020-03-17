aea56227aa758723a1a3555b9ed44b6a74270d4d
vandemjh <jack.vandem@gmail.com>
2020-03-17 02:16:34 -0400

    updated to scrape links from most included links of a page

--- scrape.py
+++ scrape.py
@@ -5,26 +5,101 @@ import requests
 import json
 import os
 import math
+import re
 
 
-# airplane = open("airplane.json","w")
+airplane = open("airplane.json","w")
 
-# toReturn = {}
+# Old version of this code, only available while on the flight
 # for i in range(100):
 #     URL = "https://www.aainflight.com/media/" + str(i)
-#     page = requests.get(URL)
-#     if (page.status_code == 200):
-#         contentSplit = str(page.content).split()
-#         for word in contentSplit:
-#             # This is hacky
-#             for inWord in word.split("\n"):
-#                 for inInWord in inWord.split("\t"):
-#                     if not word in toReturn:
-#                         toReturn[inInWord] = 1
-#                     else:
-#                         toReturn[inInWord] += 1
+    # page = requests.get(URL)
+    # if (page.status_code == 200):
+    #     contentSplit = str(page.content).split()
+    #     for word in contentSplit:
+    #         # This is hacky
+    #         for inWord in word.split("\n"):
+    #             for inInWord in inWord.split("\t"):
+    #                 if not word in toReturn:
+    #                     toReturn[inInWord] = 1
+    #                 else:
+    #                     toReturn[inInWord] += 1
 
-toReturn = json.loads(open("airplane.json","r").read())
+"""
+Finds the URL in the included string
+string[string.index("http"):len(string) - 1 - (string[::-1].index("\""))]
+  ^          ^        ^          ^        ^          ^             ^
+slices       |        |          |        |          |             |
+       string from    |          |        |          |             |
+               index of "http"   |        |          |             |
+                             to length    |          |             |
+                                 (index starts at 0) |             |
+                                               of reversed         |
+                                                         index of last quote
+"""
+def findURL(string):
+    if (isinstance(string, str)):
+        return string[string.index("http"):len(string) - 1 - (string[::-1].index("\""))]
+
+"""
+Did not add recursion to this on purpose so I would not return all of the internet
+"""
+def getLinks(URL):
+    ALLOW_OUTSIDE_URLS = False
+    toReturn = []
+    try: # Handles improper links
+        page = requests.get(URL)
+        if (page.status_code == 200):
+            for space in str(page.content).split(" "):
+                if (URL in space or ALLOW_OUTSIDE_URLS): # and URL in space for only on this domain
+                    newURL = findURL(space)
+                    if (newURL != URL): # check for self loops
+                        toReturn.append(newURL)
+            # for space in str(page.content).split("<a"): # account for anchors
+            #     if (URL in space): # and URL in space for only on this domain
+            #         toReturn.append(findURL(space))
+    except:
+        print("Improper link: " + str(URL))
+    finally:
+        return toReturn
+        # return list(filter(lambda x: requests.get(x).status_code == 200, toReturn))
+        # Filters out non-links
+
+URL = "https://www.aa.com/"
+# print((getLinks(URL)))
+allPages = []
+for link in getLinks(URL):
+    allPages.append(link)
+    for link1 in getLinks(link):
+        allPages.append(link1)
+        # print(getLinks(link1))
+        # For more depth, add more loops here
+
+toReturn = {}
+
+for URL in allPages:
+    try:
+        page = requests.get(URL)
+    except: # Handles improper links that have slipped through the cracks
+        page.status_code = 500
+    if (page.status_code == 200):
+        contentSplit = str(page.content).split()
+        for word in contentSplit:
+            # This is hacky
+            for inWord in word.split("\n"):
+                for inInWord in inWord.split("\t"):
+                    if not word in toReturn:
+                        toReturn[inInWord] = 1
+                    else:
+                        toReturn[inInWord] += 1
+
+json.dump(toReturn, airplane)
+
+airplane.close()
+# quit() # Remove later
+airplane = open("airplane.json","r")
+
+toReturn = json.loads(airplane.read())
 
 largestVal = 0
 for val in toReturn.values():
@@ -39,10 +114,11 @@ for i in range(len(toReturn.values())):
     valueRange.append(i)
 
 sortedValues = list(toReturn.values())
+sortedKeys = list(toReturn.keys())
 sortedValues.sort(reverse = True)
 
-stopPoint = 100 if len(sortedValues) > 100 else len(sortedValues)# math.floor(len(valueRange) * .1)
-stopPercent = 100 / len(sortedValues)
+stopPoint = 60 if len(sortedValues) > 60 else len(sortedValues)# math.floor(len(valueRange) * .1)
+stopPercent = 60 / len(sortedValues)
 
 try:
     import matplotlib.pyplot as plt
@@ -71,3 +147,5 @@ ax.set_title("Log Log Graph of Code fragements by number of occurances")
 # plt.show()
 plt.savefig("Log Log Graph of Code fragements by number of occurances")
 plt.close(fig)
+
+print("Most used code fragement: \"" + str(list(toReturn.keys())[list(toReturn.values()).index(sortedValues[0])]) + "\" used " + str(sortedValues[0]) + " times!")
